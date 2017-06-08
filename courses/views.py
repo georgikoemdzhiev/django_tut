4bf9da6f5e8258b9340dad299a2862559a9247fc
boggogo <koemdjiev@gmail.com>
2017-06-08 20:10:46 +0100

    Added content field to the Step model

--- courses/views.py
+++ courses/views.py
@@ -3,7 +3,7 @@
 
 from django.shortcuts import render, get_object_or_404
 
-from . models import Course
+from . models import Course, Step
 # Create your views here.
 
 def course_list(request):
@@ -19,6 +19,9 @@ def my_method(request):
 
 def course_detail(request, pk):
     course = get_object_or_404(Course, pk=pk)
-    return render(request,
-                  'courses/course_detail.html',
-                   {'course':course})
+    return render(request, 'courses/course_detail.html', {'course':course})
+
+
+def step_detail(request, course_pk, step_pk):
+    step = get_object_or_404(Step, course_id=course_pk, pk=step_pk)
+    return render(request, 'courses/step_detail.html', {'step':step})
